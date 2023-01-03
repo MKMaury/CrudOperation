c34ce462b381e1661a6d16351f4bc40beec2a0dd
MKMaury <mukesh.kumar@pieinfosystems.com>
2023-01-03 05:02:48 -0800

    crud operations

---
+++ PieInfo/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PieInfo.Models;
+
+namespace PieInfo.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options) 
+            : base(options) 
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
